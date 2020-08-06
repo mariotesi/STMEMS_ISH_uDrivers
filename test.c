6f59af530c76363266944a88e05e9ee1ee9024ce
Mario Tesi <mario.tesi@st.com>
2020-08-06 10:32:08 +0200

    iAdded simple test function
    Signed-off-by: Mario Tesi <mario.tesi@st.com>

---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <libgen.h>
+#include <errno.h>
+#include <string.h>
+#include <getopt.h>
+#include <sys/types.h>
+
+int main(int argc, char *argv[])
+{
+	printf("Hello World !\n");
+
+	return 0;
+}
+
